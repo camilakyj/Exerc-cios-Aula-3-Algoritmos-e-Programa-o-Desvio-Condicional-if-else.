230f41e7e266d9c03cac18a98020c9609108fcfa
Camila <yagimaj@gmail.com>
2026-03-11 19:21:55 -0300

    Add files via upload

---
+++ Exercicio8Aula3.java
@@ -0,0 +1,26 @@
+// Camila Aki Yagima Jeronimo
+package dobackofront;
+import java.util.Scanner;
+
+public class Exercicio8Aula3 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        String senha;
+
+        System.out.println("Digite a senha:");
+        senha = entrada.nextLine();
+
+        if (senha.equals("R10p5")) {
+            System.out.println("Acesso concedido");
+        } else {
+            System.out.println("Acesso negado");
+        }
+
+        entrada.close();
+    }
+
+        }
+
+
